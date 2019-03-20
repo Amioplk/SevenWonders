17e14da8b7cac09918cddbb13db003851566b0a5
Amioplk <amir.worms@dauphine.eu>
2019-03-20 21:03:51 +0100

    Fix setup

--- Action.java
+++ Action.java
@@ -1,3 +1,7 @@
+import java.util.*;
+
+
+
 abstract class Action {
   // On veut modeliser une action comme quelque chose qui peut etre lancé et qui a un effet sur les regles et sur l'etat du jeu et les joueurs
   // Une action est la pose d'une carte qui possede un effet, PAS le fait de payer les couts par exemple !
