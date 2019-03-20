b35ef9d8df879736ce7602c40593c014f6fe5cbb
Amioplk <amir.worms@dauphine.eu>
2019-03-20 20:38:35 +0100

    Commit new ideas

---
+++ SevenWondersIdee.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+// *************************************************************************7Wonders***********************************************************************************
+
+// L'approche doit se faire par les actions et determiner si oui ou non les coups sont legaux
+/**
+On a 4 facteurs qui influent sur notre modelisation du jeu:
+  - Etat du jeu
+  - Joueurs
+  - Actions
+  - Regles
+*/
+
+
+
+/*public class Pair<L,R> {
+
+  private final L left;
+  private final R right;
+
+  public Pair(L left, R right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  public L getLeft() { return left; }
+  public R getRight() { return right; }
+  public void setLeft(L l) { this.left = l; }
+  public void setRight(R r) { this.right = r; }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (!(o instanceof Pair)) return false;
+    Pair pairo = (Pair) o;
+    return this.left.equals(pairo.getLeft()) &&
+           this.right.equals(pairo.getRight());
+  }
+
+}
+*/
